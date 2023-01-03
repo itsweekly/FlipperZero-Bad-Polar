f4d281d7b1aa2949e00185b7c0610a2d16650313
itsweekly <92674764+itsweekly@users.noreply.github.com>
2023-01-03 21:24:58 +0100

    Update

---
+++ Proxy/Proxy.ps1
@@ -0,0 +1,15 @@
+# Download the HTML code from the URL
+$html = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/itsweekly/FlipperZero-Bad-Polar/main/Proxy/index.html" -UseBasicParsing
+
+# Save the HTML code to a temporary file
+$tempFile = [IO.Path]::GetTempFileName() + ".html"
+[IO.File]::WriteAllText($tempFile, $html.Content)
+
+# Open the temporary file in Google Chrome
+Start-Process "chrome.exe" $tempFile
+
+# Wait for 2 seconds
+Start-Sleep -Seconds 2
+
+# Delete the temporary file
+Remove-Item $tempFile
